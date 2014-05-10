2a507295e2360e81c4444aa3407ffb203355e3a4
Abhishek <dalek2point3@gmail.com>
2014-05-10 15:19:12 -0400

    fixed two bugs

--- python/pointpoly.py
+++ python/pointpoly.py
@@ -1,5 +1,6 @@
 import sys
 # from shapely.geometry import Polygon, Point, MultiPoint
+from datetime import datetime
 import timeit
 import ogr
 import csv
@@ -34,24 +35,6 @@ index_msa = lyr_msa.GetLayerDefn().GetFieldIndex("GEOID10")
 #print len(lyr_msa), index_msa
 ##
 
-
-def getlayer(filename, fieldname):
-    # load the shape file as a layer
-    drv = ogr.GetDriverByName('ESRI Shapefile')
-    ds_in = drv.Open(filename)
-
-    if ds_in is None:
-        print 'Could not open %s' % (filename)
-    else:
-        print 'Opened %s' % (filename)
-        lyr_in = ds_in.GetLayer(0)
-        featureCount = lyr_in.GetFeatureCount()
-        print "Number of features in %s: %d" % (os.path.basename(filename),featureCount)
-
-        # field index for which i want the data extracted 
-        index = lyr_in.GetLayerDefn().GetFieldIndex("fips")
-        return lyr_in
-
 def check(lon, lat, mode):
 
     if mode == "cty":
@@ -80,24 +63,27 @@ def writelog(logfileh, data):
 
 def geocode(line):
 
-    pt_lon = float(line['v3']) ## v3 --> lon
-    pt_lat = float(line['v4']) ## v4 --> lat
+    try:
+        pt_lon = float(line['v3']) ## v3 --> lon
+        pt_lat = float(line['v4']) ## v4 --> lat
+    except ValueError:
+        return line
 
-    # TODO: it should geocode both msa and county at once
-    fips= check(pt_lon, pt_lat, "cty")
-    geoid10= check(pt_lon, pt_lat, "msa")
-
-    print ('\t'.join([str(pt_lon), str(pt_lat), geoid10, fips]))
-
+    if line['fips'] == '' :
+        line['fips']= check(pt_lon, pt_lat, "cty")
+    if line['geoid10'] == '' :
+        line['geoid10']= check(pt_lon, pt_lat, "msa")
     return line
+
 
 def main(pointfile, outfilestub, startflag=0, step=10):
 
-    startflag = (startflag-1) * 1000
+    startflag = (startflag-1) * 100000
     outfile = outfilestub + "_" + str(startflag) + "-" + str(startflag+step) + ".csv"
     count = startflag 
     logfile = outfilestub + "_" + str(startflag) + "-" + str(startflag+step) + ".log"
     logfileh = open(logfile, "w")
+    starttime = str(datetime.now())
 
     with open(pointfile) as infileh:
 
@@ -109,28 +95,30 @@ def main(pointfile, outfilestub, startflag=0, step=10):
 
         with open(outfile, "w") as fileh:
             csvwriter = csv.DictWriter(fileh, fields, delimiter="\t")
-            
+
             for line in csvreader:
                 count += 1
                 if count < startflag + step:
 
                     if line['class'] == "both":
-                        print "already geocoded: " + line['geoid10'] + "\t" + line['fips']
+                        pass
                     else:
-                        # print "geocoding now ..."
                         line = geocode(line)
 
                     if (count % 1000) == 0:
                         stop = timeit.default_timer()
                         writelog(logfileh, str(round(stop-start,2)) + "(s) -- Finished " + str(count)) 
                     
-                    csvwriter.writerow(line)
+                    try:
+                        csvwriter.writerow(line)
+                    except TypeError:
+                        writelog(logfileh, "problem with writing line, skipped" + str(count))
 
                 elif count > startflag + step:
                     break
 
     stop = timeit.default_timer()
-    writelog(logfileh, "\n\nwrote " + str(count) + " items in " +  str(stop-start) + "(s)")
+    writelog(logfileh, "\n\nwrote " + str(count) + " items in " +  str(stop-start) + "(s) between " + starttime + " and " + str(datetime.now()))
     print "\n\nwrote " + str(count) + " items in " +  str(stop-start), "(s)"
     logfileh.close()
 
@@ -143,11 +131,14 @@ if __name__ == "__main__":
     # specify start and end points here.
     startflag = int(sys.argv[1].strip())
     step = int(sys.argv[2].strip())
+    region = sys.argv[3].strip()
 
-    print "starting"
+    pointfile = "/mnt/nfs6/wikipedia.proj/jmp/rawdata/osm/x_"+region+".csv"
+
+    print "starting at:" + str(datetime.now())
 
     # testing output file
-    outfilestub = "/mnt/nfs6/wikipedia.proj/jmp/rawdata/stash/ne"
+    outfilestub = "/mnt/nfs6/wikipedia.proj/jmp/rawdata/stash/" + region
 
     main(pointfile, outfilestub, startflag, step)    
 
