30a97aeea12b55f942bc9df2be9b3fe503322fef
Abhishek <dalek2point3@gmail.com>
2014-06-12 22:11:46 -0400

    parsin code done

--- python/osm-notes.py
+++ python/osm-notes.py
@@ -1,17 +1,40 @@
 import urllib2
+from xml.dom import minidom
 
 def get_data(url, fname):
     with open(fname, "wb") as f:
         response = urllib2.urlopen(url)
         f.write(response.read())
 
+def parse_data(fname):
+    xmldoc = minidom.parse(fname)
+    notes = xmldoc.getElementsByTagName('note')
+
+    data = []
+
+    for note in notes:
+        date = note.getElementsByTagName('date_created')[0].childNodes[0].nodeValue
+        id = note.getElementsByTagName('id')[0].childNodes[0].nodeValue
+        lat = note.attributes['lat'].value
+        lon = note.attributes['lon'].value
+        line = [date, id, lat, lon]
+        data.append(line)
+
+    print len(notes)
+    return data
+
 def main():
-    url = "http://api.openstreetmap.org/api/0.6/notes/search?q=bounds"
-    get_data(url, "test.xml")
+    closed = "-1"
+    limit = "10"
+    url = "http://api.openstreetmap.org/api/0.6/notes/search?q=bounds&closed=" + closed + "&limit=" + limit
+    # get_data(url, "test2.xml")
+    data = parse_data("test2.xml")
 
 
 if __name__=='__main__':
     main()
+
+## http://api.openstreetmap.org/api/0.6/notes?bbox=-0.65094,51.312159,0.374908,51.669148
 
 
 
